ec2cebc136f916001f48ae3baa6ff55ad4c2b8b6
Damian <damianlobos24@gmail.com>
2020-09-30 17:39:54 -0300

    player movement added

---
+++ FPS/Assets/Scripts/playerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speedMovement = 12f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speedMovement * Time.deltaTime);
+    }
+}
